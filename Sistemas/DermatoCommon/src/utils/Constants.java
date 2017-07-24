83365e6c48814fea92514df3da2acb12855cdf81
Fran <fran.visintini7@gmail.com>
2017-07-24 11:36:03 -0300

    Add medical coverages back up without Particular

--- Sistemas/DermatoCommon/src/utils/Constants.java
+++ Sistemas/DermatoCommon/src/utils/Constants.java
@@ -4,20 +4,20 @@ package utils;
  * @author Francisco Visintini
  */
 public class Constants {
-
-    //DB connection data
-    public static final String HOST = "localhost";
-    public static final String PORT = "3306";
-    public static final String USER = "root";
-    public static final String PASSWORD = "3654";
-    public static final String DB = "dermato";
-    public static final String URL = "jdbc:mysql://localhost:3306/dermato";
-
-    //File managing data
-    public static final String COLOR_FILE_PATH = "C:\\Program Files\\Sistema Dermatologico\\Archivos Auxiliares\\Color.dat";
-    public static final String AUXILIAR_AUTOMATIC_BACKUP_PATH = "C:\\Program Files\\Sistema Dermatologico\\Backup Automatico\\BackUp.dat";
-    public static final String BACKUP_PRCESS_PATH = "\"C:\\Program Files\\MySQL\\MySQL Server 5.5\\bin\\mysqldump.exe\"";
-    public static final String BACKUPS_FOLDER_PATH = "C:\\Program Files\\Sistema Dermatologico\\Backup Automatico\\";
+//
+//    //DB connection data
+//    public static final String HOST = "localhost";
+//    public static final String PORT = "3306";
+//    public static final String USER = "root";
+//    public static final String PASSWORD = "3654";
+//    public static final String DB = "dermato";
+//    public static final String URL = "jdbc:mysql://localhost:3306/dermato";
+//
+//    //File managing data
+//    public static final String COLOR_FILE_PATH = "C:\\Program Files\\Sistema Dermatologico\\Archivos Auxiliares\\Color.dat";
+//    public static final String AUXILIAR_AUTOMATIC_BACKUP_PATH = "C:\\Program Files\\Sistema Dermatologico\\Backup Automatico\\BackUp.dat";
+//    public static final String BACKUP_PRCESS_PATH = "\"C:\\Program Files\\MySQL\\MySQL Server 5.5\\bin\\mysqldump.exe\"";
+//    public static final String BACKUPS_FOLDER_PATH = "C:\\Program Files\\Sistema Dermatologico\\Backup Automatico\\";
 
     //OTHERS
     public static final String FULLNAME = "%s, %s";
@@ -26,14 +26,14 @@ public class Constants {
     public static final String SYSTEM_ICON_IMAGE_PATH = "images/system_icon.png";
     
     //DEVELOPMENT ENVIRONMENT
-//    public static final String HOST = "localhost";
-//    public static final String PORT = "3306";
-//    public static final String USER = "root";
-//    public static final String PASSWORD = "3282";
-//    public static final String DB = "Dermato";
-//    public static final String URL = "jdbc:mysql://localhost:3306/Dermato";
-//    public static final String COLOR_FILE_PATH = "/home/fran/Desktop/Color.dat";
-//    public static final String AUXILIAR_AUTOMATIC_BACKUP_PATH = "/home/fran/Desktop/BackUp.dat";
-//    public static final String BACKUP_PRCESS_PATH = "/usr/bin/mysqldump";
-//    public static final String BACKUPS_FOLDER_PATH = "/home/fran/Desktop";
+    public static final String HOST = "localhost";
+    public static final String PORT = "3306";
+    public static final String USER = "root";
+    public static final String PASSWORD = "3282";
+    public static final String DB = "Dermato";
+    public static final String URL = "jdbc:mysql://localhost:3306/Dermato";
+    public static final String COLOR_FILE_PATH = "/home/fran/Desktop/Color.dat";
+    public static final String AUXILIAR_AUTOMATIC_BACKUP_PATH = "/home/fran/Desktop/BackUp.dat";
+    public static final String BACKUP_PRCESS_PATH = "/usr/bin/mysqldump";
+    public static final String BACKUPS_FOLDER_PATH = "/home/fran/Desktop";
 }
